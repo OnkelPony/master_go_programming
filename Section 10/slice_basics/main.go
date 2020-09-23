5f002f46e3db3b3235b45469f9464de1a98cb5af
zkurvenej@outlook.com <3jaPLko8>
2020-09-23 14:01:01 +0200

    Before 83 - Slice expressions.

--- Section 10/slice_basics/main.go
+++ Section 10/slice_basics/main.go
@@ -1,1 +1,60 @@
 package main
+
+import "fmt"
+
+func main() {
+	var cities []string
+	fmt.Println("cities is equal to nil:", cities == nil)
+	fmt.Printf("cities is %#v\n", cities)
+
+	numbers := []int{2, 3, 4, 5}
+	fmt.Println(numbers)
+
+	nums := make([]int, 2)
+	fmt.Println(nums)
+
+	type names []string
+	friends := names{"Aletta", "Katsumi"}
+	fmt.Printf("%#v\n", friends)
+
+	x := numbers[0]
+	fmt.Println("x is", x)
+
+	numbers[1] = 108
+	fmt.Printf("%#v\n", numbers)
+
+	for index, value := range numbers {
+		fmt.Printf("index: %v, value: %v\n", index, value)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		fmt.Printf("index: %v, value: %v\n", i, numbers[i])
+	}
+
+	var n []int
+	n = numbers
+	_ = n
+
+	var nn []int
+	fmt.Printf("nn equals nil: %v\n", nn == nil)
+
+	mm := []int{}
+	fmt.Printf("mm equals nil: %v\n", mm == nil)
+	fmt.Printf("mm is: %#v\n", mm)
+
+	a, b := []int{1, 2, 3}, []int{1, 2, 108}
+	var eq bool = true
+	for i, valA := range a {
+		if valA != b[i] {
+			eq = false
+			break
+		}
+	}
+
+	if eq {
+		fmt.Println("a and b are equal")
+	} else {
+		fmt.Println("a and b are not equal")
+	}
+
+}
