02c482869525facb4e2a3c1cea31b9d4551cbe89
Jiří Martínek <zwo@centrum.cz>
2020-10-20 23:36:47 +0200

    Before 206. Hands-On Exercises - Exercise 4

--- Section 29/Exercise 1/main.go
+++ Section 29/Exercise 1/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "packages/mypackages/arithmetic"
+import (
+	"fmt"
+	"packages/mypackages/arithmetic"
+)
+
 func main() {
-	for i := 0; i < 108; i++ {
-		if arithmetic.
+	for i := 5; i < 108; i++ {
+		if arithmetic.IsPrime(i) {
+			fmt.Println(i)
+		}
 	}
 }
